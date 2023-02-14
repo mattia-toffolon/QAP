d1816ef6d41c46695570d3bf4eee3fc4b2e9b653
Mattia <mattia2001.toffolon@gmail.com>
2023-02-14 23:01:16 +0100

    Parameters input and matrix A generation completed

--- MatricesGenerator.py
+++ MatricesGenerator.py
@@ -1,3 +1,54 @@
-print("Hello World!")
+# Matrices Generator for a QAP problem
 
-#Test change
+while(True):
+    print("Insert the 'n' value (must be a positive integer):")
+    n = input()
+    try:
+        n = int(n)
+        if(n <= 0):
+            print("Wrong input. Retry.")
+        else:
+            break
+    except ValueError:
+        print("Wrong input. Retry.")
+
+while(True):
+    print("Insert the 'n1' and 'n2' values (must be positive integers so that n1xn2=n):")
+    n1 = input()
+    n2 = input()
+    try:
+        n1 = int(n1)
+        n2 = int(n2)
+        if(n1 <= 0 or n2 <= 0 or n1*n2 != n):
+            print("Wrong input. Retry.")
+        else:
+            break
+    except ValueError:
+        print("Wrong input. Retry.")
+
+while(True):
+    print("Insert the 'm' value (must be a positive integer so that m<=n):")
+    m = input()
+    try:
+        m = int(m)
+        if(m <= 0 or m > n):
+            print("Wrong input. Retry.")
+        else:
+            break
+    except ValueError:
+        print("Wrong input. Retry.")
+
+# test print
+print(n1, " ", n2, " ", n, " ", m)
+
+A = [0]*n
+for i in range(n):
+    if(i < m):
+        A[i] = [1]*m + [0]*(n-m)
+    else:
+        A[i] = [0]*n
+
+for row in A:
+    for val in row:
+        print(val, end=" ")
+    print()
