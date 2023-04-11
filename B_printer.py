c5e7f837f09f18b427b1af44829f13b76f97fa98
Mattia <mattia2001.toffolon@gmail.com>
2023-04-11 11:30:49 +0200

    B_printer completed

--- B_printer.py
+++ B_printer.py
@@ -4,11 +4,21 @@
 ##!/usr/bin/env python
 # encoding: utf-8
 
+import numpy as np
 import MatricesGenerator as mg
 
 n_parameters = [(16,4,4), (25,5,5), (36,6,6)]
 gray_densities = [20.0, 50.0, 80.0]
 
+# D is useless here!
 if __name__ == '__main__':
     for n in n_parameters:
         for d in gray_densities:
+            B = mg.B_generator(n[0], n[1], n[2])
+            f = open(f"Tai_c instances/Tai{n[0]}c_{n[1]}x{n[2]}_{int(d)}.txt", "w")
+            f.write(f"{n[1]} {n[2]} {round(n[0]*(d/100))}\n")
+            for i in range(B.shape[0]):
+                for j in range(B.shape[1]):
+                    f.write(f"{B[i, j]} ")
+                f.write("\n")
+            f.close()
